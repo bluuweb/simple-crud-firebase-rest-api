0614920c79cfd5c9d930182e43ca8bd8f6e1192b
bluuweb <contacto@bluuweb.cl>
2022-02-14 12:51:43 -0300

    todo simple firebase rest

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<h1>TODO APP</h1>
+		<p v-if="error">Por favor escribe algo...</p>
+		<form @submit.prevent="procesarFormulario">
+			<input
+				type="text"
+				placeholder="Ingrese todo"
+				v-model.trim="textoTodo"
+			/>
+			<button type="submit">Agregar</button>
+		</form>
+		<div v-if="arrayTodos.length === 0">¡Empieza a agregar TODOS!</div>
+		<ul>
+			<li v-for="(item, index) of arrayTodos" :key="index">
+				<span>{{ item.texto }}</span>
+				<span>{{ item }} </span>
+				<button @click="eliminarTodo(item.id)">Eliminar</button>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	data() {
+		return {
+			textoTodo: "",
+			arrayTodos: [],
+			error: false,
+		};
+	},
+	methods: {
+		async procesarFormulario() {
+			if (this.textoTodo === "") {
+				this.error = true;
+				return console.log("escribe algo por favor");
+			}
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+			this.error = false;
+
+			const objetoTodo = {
+				texto: this.textoTodo,
+				id: Date.now(),
+			};
+
+			try {
+				const res = await fetch(
+					`https://app-todo-vue-79e5f-default-rtdb.firebaseio.com/todos/${objetoTodo.id}.json`,
+					{
+						method: "put",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(objetoTodo),
+					}
+				);
+
+				if (!res.ok) return console.log("falló al agregar todo");
+
+				this.arrayTodos.push(objetoTodo);
+				this.textoTodo = "";
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		async eliminarTodo(id) {
+			try {
+				const res = await fetch(
+					`https://app-todo-vue-79e5f-default-rtdb.firebaseio.com/todos/${id}.json`,
+					{
+						method: "delete",
+					}
+				);
+
+				if (!res.ok) return console.log("Falló al intentar eliminar");
+
+				this.arrayTodos = this.arrayTodos.filter(
+					(item) => item.id !== id
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+	},
+	async created() {
+		try {
+			const res = await fetch(
+				"https://app-todo-vue-79e5f-default-rtdb.firebaseio.com/todos.json"
+			);
+			const data = await res.json();
+
+			for (const key in data) {
+				this.arrayTodos.push(data[key]);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	},
+};
+</script>
